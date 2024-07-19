a11a336820991a8e847d334a8a6fec178859c9a7
Abhay-Rana-18 <ranaabhay795@gmail.com>
2024-07-20 03:17:04 +0530

    address page improved

--- Frontend/src/Pages/Buy/Addresses.jsx
+++ Frontend/src/Pages/Buy/Addresses.jsx
@@ -150,17 +150,18 @@ const Addresses = () => {
     <>
       <div className="block md:flex lg:flex">
         <div className="addressPage">
-          <h1 className="text-2xl max-sm:text-xl mt-7 ml-5 md-ml-20">Select Address</h1>
+          <h1 className="text-2xl max-sm:text-xl mt-10 md:ml-[2rem] ml-5 font-semibold">
+            Select Address
+          </h1>
           <div className="saved">
-            <p className="ml-[3rem] mt-[2.5rem]">Saved Addresses</p>
+            <p className="ml-8 md:ml-[3rem] mt-[2.5rem]">Saved Addresses</p>
             {address.map((add) => (
               <div
-                className={`addressCard w-[87vw] bg-blue-100/50 p-5 mx-[2rem] mt-5 rounded-md border-2 border-slate-200 ${
+                className={`addressCard mx-5 p-3 bg-blue-100/50 sm:p-5 sm:mx-[2rem] mt-5 rounded-md border-2 border-slate-200 ${
                   selected === add._id ? "bg-blue-100/50" : "bg-white"
                 } md:w-[50vw] mx-[3rem]`}
                 key={add._id}
                 onClick={() => {
-                  console.log(add);
                   setSelected(add._id);
                 }}
               >
@@ -173,27 +174,29 @@ const Addresses = () => {
                   <h3 className="text-xl max-sm:text-lg">{add.place}</h3>
                   {selected === add._id ? (
                     <>
-                      <img
-                        src={correct}
-                        alt="#"
-                        className="w-7 block ml-auto"
-                      />
+                      <div className="circle bg-transparent ml-auto w-6 h-6">
+                        <img
+                          src={correct}
+                          alt="#"
+                          className="w-6 block ml-auto"
+                        />
+                      </div>
                     </>
                   ) : (
                     <div className="circle ml-auto bg-white border-2 border-slate-300 w-6 h-6"></div>
                   )}
                 </div>
-                <div className="flex py-2">
+                <div className="flex pt-1 pr-3 text-sm md:text-[0.9rem] md:py-2 md:px-4">
                   <p>
                     {add.address}, {add.city}, {add.state}, {add.pincode},{" "}
                     {add.country}, {add.phone}
                   </p>
-                  <div className="flex gap-3 ml-auto">
+                  <div className="flex gap-2 ml-auto">
                     <p
                       onClick={() => {
                         handleDelete(add._id);
                       }}
-                      className="underline cursor-pointer"
+                      className="underline cursor-pointer px-2 hover:text-red-600"
                     >
                       Delete
                     </p>
@@ -210,7 +213,7 @@ const Addresses = () => {
           </div>
 
           <div
-            className="new w-[87vw] bg-slate-50 p-5  mt-5 rounded-md border-2 border-slate-200 md:w-[50vw] mx-[3rem]"
+            className="new mx-8 bg-slate-50 p-5 mt-5 rounded-md border-2 border-slate-200 md:w-[50vw]"
             onClick={handleCreate}
           >
             <div className="flex">
@@ -220,16 +223,13 @@ const Addresses = () => {
           </div>
         </div>
 
-        <div className="productDetails mt-20 w-[30vw] md:ml-auto lg:ml-auto sm:ml-[4.6rem] max-sm:ml-5">
-          <h1 className="lg:hidden md:hidden sm:block text-2xl mb-5 max-sm:text-lg">
+        <div className="productDetails mt-20 max-sm:ml-5 ml-auto">
+          <h1 className="lg:hidden md:hidden sm:block text-2xl mb-5 max-sm:text-lg font-semibold">
             Bill Details:
           </h1>
           {id && (
-            <div className="productDetails mt-20 w-[30vw] md:ml-auto lg:ml-auto sm:ml-[4.6rem] max-sm:ml-5">
-              <h1 className="lg:hidden md:hidden sm:block text-2xl mb-5 max-sm:text-lg">
-                Bill Details:
-              </h1>
-              <div className="card p-5 w-[17rem] m-auto my-3 border-slate-200 border-2">
+            <div className="productDetails mt-3 md:ml-auto sm:ml-[4.6rem] ml-1">
+              <div className="card p-5 m-auto my-3 border-slate-200 border-2">
                 <img
                   src={product.thumbnail}
                   alt="#"
@@ -248,10 +248,10 @@ const Addresses = () => {
                   <p className="ml-auto">${product.price}</p>
                 </div>
               </div>
-              <div className="flex border-slate-150 border-2 w-[17rem] m-auto p-2">
+              <div className="flex border-slate-150 border-2 m-auto p-2">
                 <h1 className="text-xl ml-3 max-sm:text-lg">Total price: </h1>
                 <p className="ml-auto text-xl max-sm:text-lg">
-                  ${product.price}
+                  ${product.price?.toFixed(2)}
                 </p>
               </div>
             </div>
@@ -260,44 +260,46 @@ const Addresses = () => {
             cart?.map((product) => (
               <div
                 key={product._id}
-                className="card p-5 w-[17rem] m-auto my-3 border-slate-200 border-2"
+                className="card p-3 sm:mr-3 m-auto sm:m-3 my-3 border-slate-200 border-2 sm:flex"
               >
                 <img
                   src={product?.product.thumbnail}
                   alt="#"
-                  className="w-[12rem] h-[12rem] m-auto mb-2"
+                  className="w-[10rem] m-auto mb-2 object-cover h-[7rem]"
                 />
-                <div className="flex">
-                  <p>Name: </p>
-                  <p className="ml-auto">{product?.product.name}</p>
-                </div>
-                <div className="flex">
-                  <p>Qty: </p>
-                  <p className="ml-auto">{product.quantity}</p>
-                </div>
-                <div className="flex">
-                  <p>Discout: </p>
-                  <p className="ml-auto">
-                    {product?.product.discountPercentage}%
-                  </p>
-                </div>
-                <div className="flex">
-                  <p>Price:</p>
-                  <p className="ml-auto">${product?.total}</p>
+                <div className="p-3 m-2">
+                  <div className="flex sm:gap-3 gap-2">
+                    <p className="italic">Name: </p>
+                    <p className="ml-auto font-semibold">{product?.product.name}</p>
+                  </div>
+                  <div className="flex sm:gap-3 gap-2">
+                    <p className="italic">Qty: </p>
+                    <p className="ml-auto font-semibold">{product.quantity}</p>
+                  </div>
+                  <div className="flex sm:gap-3 gap-2">
+                    <p className="italic">Discout: </p>
+                    <p className="ml-auto font-semibold">
+                      {product?.product.discountPercentage}%
+                    </p>
+                  </div>
+                  <div className="flex sm:gap-3 gap-2">
+                    <p className="italic">Price:</p>
+                    <p className="ml-auto font-semibold">${product?.total?.toFixed(2)}</p>
+                  </div>
                 </div>
               </div>
             ))}
 
           {!id && (
             <>
-              <div className="flex border-slate-150 border-2 w-[17rem] m-auto p-2">
+              <div className="flex border-slate-150 border-2 p-2 m-3">
                 <h1 className="text-xl ml-3 max-sm:text-lg">Total price: </h1>
-                <p className="ml-auto text-xl max-sm:text-lg">${total}</p>
+                <p className="ml-auto text-xl max-sm:text-lg font-semibold pr-3">${total?.toFixed(2)}</p>
               </div>
             </>
           )}
           <button
-            className="btn mb-10 mt-10 px-4 py-2 ml-5 bg-blue-700/70 rounded-md text-white max-sm:ml-2 w-[10rem] md:ml-10 lg:ml-[4rem]"
+            className="btn px-2 py-2 bg-slate-700/70 rounded-md text-white max-sm:ml-2 w-[10rem] sm:m-3"
             onClick={handlePayment}
           >
             Proceed to buy
